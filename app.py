712a60f2b6e5fc0eac2dfccd1adafb849278dd8c
Artem <artemcool200911@gmail.com>
2025-07-15 21:06:24 +0300

    ...

--- app.py
+++ app.py
@@ -50,10 +50,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256)) # <-- ВИПРАВЛЕНО!
     is_admin = db.Column(db.Boolean, default=False)
-    reviews = db.relationship('Review', backref='author', lazy='dynamic')
-    orders = db.relationship('Order', backref='customer', lazy='dynamic')
 
     def set_password(self, password): self.password_hash = generate_password_hash(password)
 
