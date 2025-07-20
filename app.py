7638e20b3ac82c5234598bde31f07e2d678810a0
Artem <artemcool200911@gmail.com>
2025-07-20 16:04:07 +0300

    Configure database URI for production and fix imports

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.do')
 app.secret_key = os.getenv("FLASK_SECRET", "nova-secret")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
