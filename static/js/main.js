aaa3a582b83a50371f6629d1d7d3c02886ea7b12
Artem <artemcool200911@gmail.com>
2025-08-11 09:56:12 +0300

    Update new search in catalog

--- static/js/main.js
+++ static/js/main.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // [ОНОВЛЕНО] Запуск нових функцій
     initInfiniteScroll();
-    initSearchSuggestions(); // <--- ОСЬ ЦЕ ВИПРАВЛЕННЯ!
+    initSearchSuggestions(); // <--- ОСЬ ЦЕЙ РЯДОК ВСЕ ВИРІШУЄ! ЙОГО НЕ БУЛО.
 
     updateCartView();
     updateFavoritesUI();
